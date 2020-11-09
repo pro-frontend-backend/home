ad1e37fa5a17e0002c8128f652cc8d7e1ecdc5bf
Peter Rosell <peter.frontendgit@gmail.com>
2020-11-09 18:07:14 +0100

    Frontend init

---
+++ home-next/pages/api/index.ts
@@ -0,0 +1,22 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+let ssrCache = false;
+let apolloClient: any = null;
+
+const create = (initialState: any) => {
+  return new ApolloClient({
+    uri: "http://localhost:3000",
+    cache: new InMemoryCache().restore(initialState),
+  });
+};
+const initApollo = (initialState?: any) => {
+  if (!process.browser) {
+    return create(initialState);
+  }
+  if (apolloClient && !ssrCache) apolloClient.cache.restore(initialState);
+  if (!apolloClient) apolloClient = create(initialState);
+
+  return apolloClient;
+};
+
+export default initApollo;
